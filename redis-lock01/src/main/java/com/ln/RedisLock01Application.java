3af45303bcc21c0a40b73c3a9926f06b5de05e60
Zhipeng He <pp361127@163.com>
2022-01-03 22:46:55 +0800

    redis分布式锁

---
+++ redis-lock01/src/main/java/com/ln/RedisLock01Application.java
@@ -0,0 +1,19 @@
+package com.ln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author HeZhipeng
+ * @Date 2022/1/3 22:35
+ **/
+@SpringBootApplication
+public class RedisLock01Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLock01Application.class, args);
+    }
+
+}
---
+++ redis-lock02/src/main/java/com/ln/RedisLock02Application.java
@@ -0,0 +1,18 @@
+package com.ln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author HeZhipeng
+ * @Date 2022/1/3 22:38
+ **/
+@SpringBootApplication
+public class RedisLock02Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(RedisLock02Application.class, args);
+    }
+
+}
