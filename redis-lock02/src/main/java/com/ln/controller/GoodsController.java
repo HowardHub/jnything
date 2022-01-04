e9b5ed7cd497fc9c1268dfafd6a09931232bc974
Zhipeng He <pp361127@163.com>
2022-01-05 00:14:12 +0800

    redis分布式锁-4.0版，加入分布式锁后，必须保证锁能被最终释放，即使出现异常。

--- redis-lock02/src/main/java/com/ln/controller/GoodsController.java
+++ redis-lock02/src/main/java/com/ln/controller/GoodsController.java
@@ -32,24 +32,28 @@ public class GoodsController {
     @GetMapping("/buyGoods")
     public String buyGoods() {
         String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
-        Boolean flag = redisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);// 等价于SETNX
-        if (!flag) {
-            return "抢锁失败";
-        }
-        String result = redisTemplate.opsForValue().get("goods:001");// get(key) ==> 看看库存够不够
-        int goodsNumber = result == null ? 0 : Integer.parseInt(result);
-        if (goodsNumber > 0) {
-            int realNumber = goodsNumber - 1;
-            redisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
-            redisTemplate.delete(REDIS_LOCK); // 释放锁
-            log.info(String.format("从【%s端口】成功买到一个商品，库存还剩下%d件", serverPort, realNumber));
-            return String.format("从【%s端口】成功买到一个商品，库存还剩下%d件", serverPort, realNumber);
-        } else {
-            return "商品已经售完.......+ 服务提供端口：" + serverPort;
-        }
 
+        try {
+            Boolean flag = redisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);// 等价于SETNX
+            if (!flag) {
+                return "抢锁失败";
+            }
+            String result = redisTemplate.opsForValue().get("goods:001");// get(key) ==> 看看库存够不够
+            int goodsNumber = result == null ? 0 : Integer.parseInt(result);
+            if (goodsNumber > 0) {
+                int realNumber = goodsNumber - 1;
+                redisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
+                log.info(String.format("从【%s端口】成功买到一个商品，库存还剩下%d件", serverPort, realNumber));
+                return String.format("从【%s端口】成功买到一个商品，库存还剩下%d件", serverPort, realNumber);
+            } else {
+                return "商品已经售完.......+ 服务提供端口：" + serverPort;
+            }
+        } finally {
+            redisTemplate.delete(REDIS_LOCK); // 释放锁
+        }
 
     }
+
 
 
 
